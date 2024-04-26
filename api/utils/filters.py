673e95bfa347b8bbe118099d1bb624024dc3efa3
Rodrigo Costa <rodrigo.costa@gmail.com>
2024-04-26 18:16:40 -0300

    refactoring unused files

--- api/utils/filters.py
+++
@@ -1,6 +0,0 @@
-# app/utils/filters.py
-def apply_filters(query, model, filters):
-    for field, value in filters.items():
-        if hasattr(model, field):
-            query = query.filter(getattr(model, field) == value)
-    return query
